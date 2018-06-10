c03af1a527f761aee6b015306822c1efe5673562
Young Nam <chulman444@gmail.com>
2018-06-10 16:43:41 +0900

    Init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+def main():
+    
+def getClientId():
+    output = ""
+    with open("client_id.txt") as f:
+        output = f.read() 
+
+    return output
+
+if __name__=="__main__":
+    main()
