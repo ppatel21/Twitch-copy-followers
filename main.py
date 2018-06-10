7462f0776c3356335f87d597e0df1ee583377de7
Young Nam <chulman444@gmail.com>
2018-06-11 00:23:32 +0900

    Copy followers
    Tested with my one of accounts

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import requests
 import argparse
+import json
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('login_name')
+    parser.add_argument('oauth_token')
 
     args = parser.parse_args()
 
     login_name = args.login_name
     login_id = getLoginId(login_name)
+    oauth_token = args.oauth_token
     
     print("Login id : {}".format(login_id))
+
+    data = getFollowingAll(login_id)
+    print("data : {} ... len: {}".format(data, len(data)))
+
+    target_ids = list(map(lambda x: x["to_id"], data))
+
+    for target_id in target_ids:
+        followWithUserId(target_id, oauth_token)
+
+def followWithUserId(user_id, oauth_token):
+    payload = [{
+    "variables":{"input":{"disableNotifications":False,"targetID":"{}".format(user_id)}},"extensions":{},"operationName":"FollowButton_FollowUser","query":"mutation FollowButton_FollowUser($input: FollowUserInput!) {\n  followUser(input: $input) {\n    follow {\n      disableNotifications\n      __typename\n    }\n    __typename\n  }\n}\n"
+  }]
+
+    headers = {
+        "Authorization": "OAuth {}".format(oauth_token)
+    }
+    r = requests.post('https://gql.twitch.tv/gql', data=json.dumps(payload), headers=headers)
+    response = r.json()
+
+    print("FollowWithUserId response: {}".format(response))
 
 def getLoginId(login_name):
     user_info = getUsers(login_name)
@@ -18,9 +42,8 @@ def getLoginId(login_name):
     return output
 
 def getUsers(login_name):
-    client_id = getClientId()
-    headers = { "Client-ID": client_id }
-    r = requests.get('https://api.twitch.tv/helix/users?login={}'.format(login_name), headers=headers)
+    headers = getAuthHeader()
+    r = requests.get('https://api.twitch.tv/helix/users?login={}&scope=user:read:email'.format(login_name), headers=headers)
     output = r.json()
     print("getUsers output : {}".format(output))
     return output
@@ -33,6 +56,35 @@ def getClientId():
     # Strip, else `\n` gets appended and the Twitch API responds with error.
     output = output.strip()
     return output
+
+def getAuthHeader():
+    client_id = getClientId()
+    headers = { "Client-ID": client_id }
+    return headers
+
+def getFollowing(login_id):
+    headers = getAuthHeader()
+    r = requests.get('https://api.twitch.tv/helix/users/follows?from_id={}'.format(login_id), headers=headers)
+    output = r.json()
+    print("getFollowing output : {}".format(output))
+    return output
+
+def getFollowingAll(login_id):
+    headers = getAuthHeader()
+    data = []
+
+    cursor = ""
+    while(True):
+        r = requests.get('https://api.twitch.tv/helix/users/follows?from_id={}&after={}'.format(login_id, cursor), headers=headers)
+        response = r.json()
+        cursor = response["pagination"]["cursor"]
+        
+        if len(response["data"]) == 0:
+            break 
+    
+        data += response["data"]
+
+    return data
 
 if __name__=="__main__":
     main()
